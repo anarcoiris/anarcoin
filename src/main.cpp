aa3d2584bd095dd31ca84e1bac922952193dd9ba
anarcoiris <36089306+anarcoiris@users.noreply.github.com>
2018-06-20 21:09:11 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ map<uint256, set<uint256> > mapOrphanTransactionsByPrev;
 // Constant stuff for coinbase transactions we create:
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Ignition Signed Message:\n";
+const string strMessageMagic = "Anarcoin Signed Message:\n";
 
 std::set<uint256> setValidatedTx;
 
@@ -2575,7 +2575,7 @@ bool CBlock::CheckBlock(bool fCheckPOW, bool fCheckMerkleRoot, bool fCheckSig) c
 
                     CTxDestination address1;
                     ExtractDestination(payee, address1);
-                    CIgnitioncoinAddress address2(address1);
+                    CAnarcoincoinAddress address2(address1);
 
                     if(!foundPaymentAndPayee) {
                         if(fDebug) { LogPrintf("CheckBlock() : Couldn't find masternode payment(%d|%d) or payee(%d|%s) nHeight %d. \n", foundPaymentAmount, masternodePaymentAmount, foundPayee, address2.ToString().c_str(), pindexBest->nHeight+1); }
@@ -3276,7 +3276,7 @@ struct CImportingNow
 
 void ThreadImport(std::vector<boost::filesystem::path> vImportFiles)
 {
-    RenameThread("Ignition-loadblk");
+    RenameThread("Anarcoin-loadblk");
 
     CImportingNow imp;
 
