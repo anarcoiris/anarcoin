54946bbff7ae90deb80d778325c7ff9442e49f95
anarcoiris <36089306+anarcoiris@users.noreply.github.com>
2018-06-20 20:02:11 +0200

    Update hash.h

--- src/hash.h
+++ src/hash.h
@@ -16,7 +16,7 @@
 
 #include <vector>
 
-/** A hasher class for Ignition's 256-bit hash (double SHA-256). */
+/** A hasher class for Anarcoin's scrypt hash. */
 class CHash256 {
 private:
     CSHA256 sha;
@@ -40,7 +40,7 @@ public:
     }
 };
 
-/** A hasher class for Ignition's 160-bit hash (SHA-256 + RIPEMD-160). */
+/** A hasher class for Anarcoin's 160-bit hash (SHA-256 + RIPEMD-160). */
 class CHash160 {
 private:
     CSHA256 sha;
